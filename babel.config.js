042c27a37528f3343993bd76faaaec407c5e081e
Jérémie Girault <jeremie.girault@gmail.com>
2020-02-06 09:17:04 +0100

    remove unused babel syntax

--- babel.config.js
+++ babel.config.js
@@ -20,7 +20,6 @@ module.exports = (api) => {
       useESModules: !useCommonJs
     } ],
     '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-json-strings'
   ]
